02adf2718ff74c9bd601b21e78e9f8ca6ce238fb
jaldabaoth-code <grialazurabi@gmail.com>
2021-04-15 11:17:57 +0200

    corrections de header pour responsive

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,6 +21,11 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->twig->render('Home/index.html.twig');
+        return $this->twig->render('Home/index.html.twig') . $this->acceuil();
+    }
+
+    public function acceuil()
+    {
+        return $this->twig->render('Home/acceuil.html.twig');
     }
 }
