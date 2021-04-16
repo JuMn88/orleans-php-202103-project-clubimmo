3784c79cca90fcc043ba6d13b4b5ec90dc10b4ee
jaldabaoth-code <grialazurabi@gmail.com>
2021-04-16 08:17:04 +0200

    corrections des erreurs: correction de db.php, supression des fichiers annonces, correction de navbar(plus de media queries), menu recherche modifier

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,6 @@ class HomeController extends AbstractController
 {
     public function index()
     {
-        return $this->twig->render('Home/index.html.twig') . $this->accueil();
-    }
-
-    public function accueil()
-    {
-        return $this->twig->render('Home/accueil.html.twig');
+        return $this->twig->render('Home/index.html.twig');
     }
 }
