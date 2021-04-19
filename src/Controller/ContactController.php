c6f184c0883b5a6ea002d2286948624540692ca8
Thuydieutran <thuy.dieutran86@gmail.com>
2021-04-19 11:43:42 +0200

    Created and stylized the form of the Contact Page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,8 +6,9 @@
  * Date: 08/04/19
  * Time: 18:40
  */
- 
+
 namespace App\Controller;
+use App\Model\ContactManager;
 
 class ContactController extends AbstractController
 {
@@ -19,6 +20,7 @@ class ContactController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
+    
     public function index()
     {
         return $this->twig->render('Contact/index.html.twig');
