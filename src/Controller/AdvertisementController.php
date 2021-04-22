e17ed04a29d252ca679a395d905cfc5a1b5b3ab1
jaldabaoth-code <grialazurabi@gmail.com>
2021-04-22 18:26:53 +0200

    corrections

--- src/Controller/AdvertisementController.php
+++ src/Controller/AdvertisementController.php
@@ -8,25 +8,9 @@ class AdvertisementController extends AbstractController
 {
     public function index()
     {
-        $photosAdress = $this->carousel();
-        return $this->twig->render('Advertisement/index.html.twig', ['photosAdress' => $photosAdress]);
-    }
-
-    public function carousel()
-    {
         $photoManager = new PhotoManager();
         $photos = $photoManager->selectAll();
-        $photosAdress = [];
 
-        foreach ($photos as $photosInformations) {
-            if (empty($photosInformations['url'])) {
-            } else {
-                $photosAdress[] = $photosInformations['url'];
-            }
-        }
-        if (empty($photosAdress)) {
-            $photosAdress[] = "/assets/images/no_image.png";
-        }
-        return $photosAdress;
+        return $this->twig->render('Advertisement/index.html.twig', ['photos' => $photos]);
     }
 }
