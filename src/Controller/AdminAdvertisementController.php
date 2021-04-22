a22d7c3cafb50b8edc0120ee61fd636065924a12
JuMn88 <moineau.julien@hotmail.fr>
2021-04-22 17:35:56 +0200

    index and add pages for admin

--- src/Controller/AdminAdvertisementController.php
+++ src/Controller/AdminAdvertisementController.php
@@ -6,6 +6,8 @@ use App\Model\PropertyManager;
 
 class AdminAdvertisementController extends AbstractController
 {
+    public const MAX_FIELD_LENGTH = 100;
+
     public function index(): string
     {
         $propertyManager = new PropertyManager();
@@ -14,5 +16,42 @@ class AdminAdvertisementController extends AbstractController
         return $this->twig->render('Admin/Advertisement/index.html.twig', [
             'properties' => $properties,
         ]);
+    }
+    public function add(): string
+    {
+        $errors = $property = [];
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $property = array_map('trim', $_POST);
+
+            if (empty($property['reference'])) {
+                $errors[] = 'Champ référence requis';
+            }
+            if (strlen($property['reference']) > self::MAX_FIELD_LENGTH) {
+                $errors[] = 'La référence doit faire moins de ' . self::MAX_FIELD_LENGTH . ' caractères';
+            }
+            if (empty($property['price'])) {
+                $errors[] = 'Champ prix requis';
+            }
+            if ($property['price'] <= 0) {
+                $errors[] = 'Le prix doit être supérieur à 0';
+            }
+            if (empty($property['surface'])) {
+                $errors[] = 'Champ surface requis';
+            }
+            if ($property['surface'] <= 0) {
+                $errors[] = 'La surface doit être supérieure à 0';
+            }
+            if (empty($errors)) {
+                $propertyManager = new PropertyManager();
+                $propertyManager->insert($property);
+
+                header('Location: /adminAdvertisement/index');
+            }
+        }
+        return $this->twig->render('Admin/Advertisement/add.html.twig', [
+            'errors' => $errors,
+            'property' => $property,
+            ]);
     }
 }
