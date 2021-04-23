54a1ea95792a19d1f350a218e808ad59c86070d1
JuMn88 <moineau.julien@hotmail.fr>
2021-04-23 20:37:46 +0200

    deleting the adding part"

--- src/Controller/AdminAdvertisementController.php
+++ src/Controller/AdminAdvertisementController.php
@@ -17,41 +17,4 @@ class AdminAdvertisementController extends AbstractController
             'properties' => $properties,
         ]);
     }
-    public function add(): string
-    {
-        $errors = $property = [];
-
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            $property = array_map('trim', $_POST);
-
-            if (empty($property['reference'])) {
-                $errors[] = 'Champ référence requis';
-            }
-            if (strlen($property['reference']) > self::MAX_FIELD_LENGTH) {
-                $errors[] = 'La référence doit faire moins de ' . self::MAX_FIELD_LENGTH . ' caractères';
-            }
-            if (empty($property['price'])) {
-                $errors[] = 'Champ prix requis';
-            }
-            if ($property['price'] <= 0) {
-                $errors[] = 'Le prix doit être supérieur à 0';
-            }
-            if (empty($property['surface'])) {
-                $errors[] = 'Champ surface requis';
-            }
-            if ($property['surface'] <= 0) {
-                $errors[] = 'La surface doit être supérieure à 0';
-            }
-            if (empty($errors)) {
-                $propertyManager = new PropertyManager();
-                $propertyManager->insert($property);
-
-                header('Location: /adminAdvertisement/index');
-            }
-        }
-        return $this->twig->render('Admin/Advertisement/add.html.twig', [
-            'errors' => $errors,
-            'property' => $property,
-            ]);
-    }
 }
--- src/Model/PropertyManager.php
+++ src/Model/PropertyManager.php
@@ -5,16 +5,4 @@ namespace App\Model;
 class PropertyManager extends AbstractManager
 {
     public const TABLE = 'property';
-
-    public function insert(array $property): void
-    {
-        $query = "INSERT INTO " . self::TABLE . " (`reference`, `price`, `surface`)
-                VALUES (:reference, :price, :surface)";
-        $statement = $this->pdo->prepare($query);
-        $statement->bindValue('reference', $property['reference'], \PDO::PARAM_INT);
-        $statement->bindValue('price', $property['price'], \PDO::PARAM_INT);
-        $statement->bindValue('surface', $property['surface'], \PDO::PARAM_INT);
-
-        $statement->execute();
-    }
 }
