47f3e0e205dbcfa4738143e6e4788bf2c1cc45b8
JuMn88 <moineau.julien@hotmail.fr>
2021-04-27 19:22:39 +0200

    form page in admin to add a new advertisement

--- src/Model/PropertyManager.php
+++ src/Model/PropertyManager.php
@@ -8,12 +8,12 @@ class PropertyManager extends AbstractManager
 
     public function insert(array $property): void
     {
-        $query = "INSERT INTO " . self::TABLE . " (`reference`, `price`, `surface`)
-                VALUES (:reference, :price, :surface)";
+        $query = "INSERT INTO " . self::TABLE . " (`reference`, `surface`, `price`)
+                VALUES (:reference, :surface, :price)";
         $statement = $this->pdo->prepare($query);
         $statement->bindValue('reference', $property['reference'], \PDO::PARAM_STR);
-        $statement->bindValue('price', $property['price'], \PDO::PARAM_INT);
         $statement->bindValue('surface', $property['surface'], \PDO::PARAM_INT);
+        $statement->bindValue('price', $property['price'], \PDO::PARAM_INT);
 
         $statement->execute();
     }
