a7cc6384fa3a4ae3fe86b38b161203033af67107
jaldabaoth-code <grialazurabi@gmail.com>
2021-04-27 08:40:40 +0200

    save

--- database.sql
+++ database.sql
@@ -13,6 +13,8 @@ insert into photo (name, url) values ("exemple_photo_3", "https://images.unsplas
 insert into photo (name, url) values ("exemple_photo_4", "https://images.unsplash.com/photo-1595256858804-ad34bc0ce444?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2089&q=80");
 
 
+
+
 /* Create table property */
 CREATE TABLE property (
     id INT NOT NULL AUTO_INCREMENT,
@@ -26,13 +28,6 @@ CREATE TABLE property (
     city varchar(255),
     photo varchar(255),
     CONSTRAINT C6 PRIMARY KEY (id));
-
-
-
-
-
-
-
 
 INSERT INTO property (property_type, price, surface, nb_pieces, photo, sector, city, energy_performance_diagnostic, greenhouse_gas) VALUES 
 ("Maison", 200000, 100, 4,"https://images.unsplash.com/photo-1565437383733-25a07431f131?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80",'Cigogne','Orleans', "A", "B"), 
