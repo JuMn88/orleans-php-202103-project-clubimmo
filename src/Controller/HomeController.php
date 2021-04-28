379f9c6cbf7ecfb2688eec5086f06f7671899d9e
jfillion12394 <jfillion12394@gmail.com>
2021-04-28 19:35:12 +0200

    maj S03US09 CAROUSEL ACCUEIL

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace App\Controller;
+
 use App\Model\PhotoManager;
 use App\Model\PropertyManager;
 
 class HomeController extends AbstractController
 {
-
-
     public function index()
     {
         //Special 3-fold eye-catcher slider in home page:
@@ -17,26 +16,31 @@ class HomeController extends AbstractController
         $photos2 = $photoManager->selectByPropertyId($this->getPicsMidSlider()); //middle carousel pics
         $photos3 = $photoManager->selectByPropertyId($this->getPicRightSlider()); //rightmost carousel pics
 
+        //retrieve slider card information data
         $propertyManager = new PropertyManager();
-        $property1 = $propertyManager->selectOneById($this->getPicsLeftSlider()); //retrieve leftmost slider prop info data
-        $property2 = $propertyManager->selectOneById($this->getPicsMidSlider()); //retrieve middle slider prop info data
-        $property3 = $propertyManager->selectOneById($this->getPicRightSlider()); //retrieve rightmost slider prop info data
+        $property1 = $propertyManager->selectOneById($this->getPicsLeftSlider());
+        $property2 = $propertyManager->selectOneById($this->getPicsMidSlider());
+        $property3 = $propertyManager->selectOneById($this->getPicRightSlider());
 
-        return $this->twig->render('Home/index.html.twig', ['photos' => $photos,'photos2' => $photos2,'photos3' => $photos3, 'property1' => $property1,'property2' => $property2,'property3' => $property3, ]);
+        //render page settings
+        $page = 'Home/index.html.twig';
+        $paramPhotos  = ['photos' => $photos,'photos2' => $photos2,'photos3' => $photos3];
+        $paramsProperty = ['property1' => $property1,'property2' => $property2,'property3' => $property3];
+        return $this->twig->render($page, array_merge($paramPhotos, $paramsProperty));
     }
 
 
-    public function getPicsLeftSlider():int
+    public function getPicsLeftSlider(): int
     {
        //TO DO:aller chercher dans base de données
         return 1;
     }
-    public function getPicsMidSlider():int
+    public function getPicsMidSlider(): int
     {
        //TO DO:aller chercher dans base de données
         return 2;
     }
-    public function getPicRightSlider():int
+    public function getPicRightSlider(): int
     {
         //TO DO:aller chercher dans base de données
         return 3;
