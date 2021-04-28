42c48467703e54e4960c00516977de84c3d69921
jfillion12394 <jfillion12394@gmail.com>
2021-04-28 19:45:16 +0200

    fix erreurs commit s03us09

--- src/Model/PropertyManager.php
+++ src/Model/PropertyManager.php
@@ -5,4 +5,16 @@ namespace App\Model;
 class PropertyManager extends AbstractManager
 {
     public const TABLE = 'property';
+
+    public function insert(array $property): void
+    {
+        $query = "INSERT INTO " . self::TABLE . " (`reference`, `surface`, `price`)
+                VALUES (:reference, :surface, :price)";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('reference', $property['reference'], \PDO::PARAM_STR);
+        $statement->bindValue('surface', $property['surface'], \PDO::PARAM_INT);
+        $statement->bindValue('price', $property['price'], \PDO::PARAM_INT);
+
+        $statement->execute();
+    }
 }
