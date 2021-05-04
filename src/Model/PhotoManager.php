525004f1f1b44f79b3547e408cccf5d881b6ccee
jaldabaoth-code <grialazurabi@gmail.com>
2021-05-04 09:02:57 +0200

    changed controler and model for carousel take photos by property id

--- src/Model/PhotoManager.php
+++ src/Model/PhotoManager.php
@@ -5,4 +5,12 @@ namespace App\Model;
 class PhotoManager extends AbstractManager
 {
     public const TABLE = 'photo';
+
+    public function selectByPropertyId(int $id)
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE property_id =:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetchAll();
+    }
 }
